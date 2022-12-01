6484328e231a193fa7f5e1e694091fd78d56d192
TruVortex <75190116+TruVortex@users.noreply.github.com>
2022-12-01 10:55:34 -0500

    Create Star-1.py

---
+++ 2021/Day 5/Star-1.py
@@ -0,0 +1,22 @@
+input = open('input', 'r')
+floor = [[0 for i in range(1000)] for j in range(1000)]
+vents = []
+for line in input:
+    vents.append(list(map(int, line.replace(' -> ', ',').split(','))))
+for vent in vents:
+    if vent[0] == vent[2]:
+        if vent[1] > vent[3]:
+            vent[1], vent[3] = vent[3], vent[1]
+        for i in range(vent[1], vent[3] + 1):
+            floor[vent[0]][i] += 1
+    elif vent[1] == vent[3]:
+        if vent[0] > vent[2]:
+            vent[0], vent[2] = vent[2], vent[0]
+        for i in range(vent[0], vent[2] + 1):
+            floor[i][vent[1]] += 1
+ans = 0
+for i in range(1000):
+    for j in range(1000):
+        if floor[i][j] >= 2:
+            ans += 1
+print(ans)
