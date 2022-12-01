c111ec99b90e13c698c5d581802534e8e5ca9d4d
TruVortex <75190116+TruVortex@users.noreply.github.com>
2022-12-01 09:24:24 -0500

    Create Star-1.py

---
+++ 2021/Day 2/Star-1.py
@@ -0,0 +1,12 @@
+input = open('input', 'r')
+commands = input.read().split('\n')
+print(commands)
+depth = pos = 0
+for command in commands:
+    if command[0] == 'f':
+        pos += int(command[8:])
+    elif command[0] == 'd':
+        depth += int(command[5:])
+    else:
+        depth -= int(command[3:])
+print(depth * pos)
