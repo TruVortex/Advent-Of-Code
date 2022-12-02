5efc128075011e57c702e1622a6cb70d41a723d7
TruVortex <75190116+TruVortex@users.noreply.github.com>
2022-12-02 08:45:00 -0500

    Create Star-2.py

---
+++ 2022/Day 2/Star-2.py
@@ -0,0 +1,21 @@
+input = open('input', 'r')
+ans = 0
+for line in input:
+    moves = line.split()
+    move: str
+    if moves[1] == 'X':
+        move = 'ABC'[('ABC'.find(moves[0]) + 2) % 3]
+    elif moves[1] == 'Y':
+        ans += 3
+        move = moves[0]
+    else:
+        ans += 6
+        move = 'ABC'[('ABC'.find(moves[0]) + 1) % 3]
+    if move == 'A':
+        ans += 1
+    elif move == 'B':
+        ans += 2
+    else:
+        ans += 3
+    print(ans, move)
+print(ans)
