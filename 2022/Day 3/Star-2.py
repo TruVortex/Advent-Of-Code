9e1d20528ca5973812b0f217a6c638a3e9ce46d0
TruVortex <75190116+TruVortex@users.noreply.github.com>
2022-12-03 08:52:34 -0500

    Update Star-2.py

--- 2022/Day 3/Star-2.py
+++ 2022/Day 3/Star-2.py
@@ -1,15 +1,14 @@
-from string import ascii_lowercase, ascii_uppercase
+from string import ascii_letters
 
 input = open('input', 'r')
 lines = input.read().strip().split()
 ans = 0
 for i in range(0, len(lines), 3):
-    for c in ascii_lowercase:
+    for c in ascii_letters:
         if c in lines[i] and c in lines[i + 1] and c in lines[i + 2]:
-            ans += ord(c) - ord('a') + 1
-            break
-    for c in ascii_uppercase:
-        if c in lines[i] and c in lines[i + 1] and c in lines[i + 2]:
-            ans += ord(c) - ord('A') + 27
+            if c.isupper():
+                ans += ord(c) - ord('A') + 27
+            else:
+                ans += ord(c) - ord('a') + 1
             break
 print(ans)
