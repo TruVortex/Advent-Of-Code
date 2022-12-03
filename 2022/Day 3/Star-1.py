8127a3690f6fbe550343bec5cf048c247aabdab1
TruVortex <75190116+TruVortex@users.noreply.github.com>
2022-12-03 08:55:08 -0500

    Update Star-1.py

--- 2022/Day 3/Star-1.py
+++ 2022/Day 3/Star-1.py
@@ -3,9 +3,8 @@ from string import ascii_letters
 input = open('input', 'r')
 ans = 0
 for line in input:
-    first, second = line[:len(line) // 2], line[len(line) // 2:]
     for c in ascii_letters:
-        if c in first and c in second:
+        if c in line[:len(line) // 2] and c in line[len(line) // 2:]:
             if c.isupper():
                 ans += ord(c) - ord('A') + 27
             else:
