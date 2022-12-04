0efc7b28d8a2b53c7c71905f8746802b7912b40f
TruVortex <75190116+TruVortex@users.noreply.github.com>
2022-12-04 11:12:23 -0500

    Create Star-2.py

---
+++ 2021/Day 7/Star-2.py
@@ -0,0 +1,10 @@
+input = open('input', 'r')
+crabs = list(map(int, input.readline().split(',')))
+ans = 1e9
+for i in range(1200):
+    cur = 0
+    for crab in crabs:
+        dist = abs(crab - i)
+        cur += dist * (dist + 1) // 2
+    ans = min(ans, cur)
+print(ans)
