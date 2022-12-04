1e1a326925dd0563074ac430c19ba9bf7fba995b
TruVortex <75190116+TruVortex@users.noreply.github.com>
2022-12-04 08:56:56 -0500

    Create Star-2.py

---
+++ 2022/Day 4/Star-2.py
@@ -0,0 +1,7 @@
+input = open('input', 'r')
+ans = 0
+for line in input:
+    nums = list(map(int, line.strip().replace(',', '-').split('-')))
+    if (nums[2] <= nums[1] <= nums[3]) or (nums[2] <= nums[0] <= nums[3]) or (nums[0] <= nums[2] <= nums[3] <= nums[1]) or (nums[2] <= nums[0] <= nums[1] <= nums[3]):
+        ans += 1
+print(ans)
