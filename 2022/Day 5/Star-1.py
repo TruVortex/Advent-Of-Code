8af375242da6893830cdeb2046b60bb2138556be
TruVortex <75190116+TruVortex@users.noreply.github.com>
2022-12-05 08:42:05 -0500

    Create Star-1.py

---
+++ 2022/Day 5/Star-1.py
@@ -0,0 +1,16 @@
+input = open('input', 'r')
+crates = [[] for i in range(100)]
+length: int
+while (line := input.readline())[1] != '1':
+    length = len(line) // 4
+    for i in range(0, len(line), 4):
+        if line[i + 1] != ' ':
+            crates[i // 4].insert(0, line[i + 1])
+input.readline()
+while (line := input.readline()) != '':
+    command = line.split()
+    for i in range(int(command[1])):
+        crates[int(command[5]) - 1].append(crates[int(command[3]) - 1][-1])
+        crates[int(command[3]) - 1].pop()
+for i in range(length):
+    print(crates[i][-1], end='')
