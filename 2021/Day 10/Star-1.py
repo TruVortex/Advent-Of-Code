c887536352b89371757941b8330ff9a3656799f5
TruVortex <75190116+TruVortex@users.noreply.github.com>
2022-12-05 17:40:02 -0500

    Create Star-1.py

---
+++ 2021/Day 10/Star-1.py
@@ -0,0 +1,16 @@
+input = open('input', 'r')
+ans = 0
+for line in input:
+    line = line.strip()
+    leave = False
+    expected = []
+    for char in line:
+        if char in '([{<':
+            expected.append({'(': ')', '[': ']', '{': '}', '<': '>'}[char])
+        else:
+            if expected and char == expected[-1]:
+                expected.pop()
+            else:
+                ans += {')': 3, ']': 57, '}': 1197, '>': 25137}[char]
+                break
+print(ans)
