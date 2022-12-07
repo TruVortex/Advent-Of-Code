f54af0a8f62f2feae74d1d9fa7f00def3c40da9c
TruVortex <75190116+TruVortex@users.noreply.github.com>
2022-12-07 09:05:18 -0500

    Create Star-2.py

---
+++ 2022/Day 7/Star-2.py
@@ -0,0 +1,37 @@
+from bisect import bisect_left
+
+input = open('input', 'r')
+pos, tree = [], {'/': [None, 0, [], {}]}
+ans, idx, cur, line = 0, 0, '', input.readline()
+while line != '':
+    if line[2] == 'c':
+        if line[5] == '.':
+            cur = tree[cur][0]
+        elif line[5] == '/':
+            cur = '/'
+        else:
+            cur = tree[cur][3][line.strip().split()[2]]
+        line = input.readline()
+    else:
+        line = input.readline()
+        while line != '' and line[0] != '$':
+            if line[0].isdigit():
+                tree[cur][1] += int(line.split()[0])
+            else:
+                tree[cur][2].append(line.strip().split()[1] + str(idx))
+                tree[cur][3][line.strip().split()[1]] = line.strip().split()[1] + str(idx)
+                tree[line.strip().split()[1] + str(idx)] = [cur, 0, [], {}]
+            line = input.readline()
+            idx += 1
+
+
+def dfs(v):
+    global tree, ans, pos
+    for u in tree[v][2]:
+        tree[v][1] += dfs(u)
+    pos.append(tree[v][1])
+    return tree[v][1]
+
+
+dfs('/')
+print(sorted(pos)[bisect_left(sorted(pos), tree['/'][1] - 40000000)])
