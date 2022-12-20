84dbe9a9f3b59a56e486219ffb55d8b0ebf5fb29
TruVortex <75190116+TruVortex@users.noreply.github.com>
2022-12-20 09:28:39 -0500

    Create Star-2.py

---
+++ 2022/Day 20/Star-2.py
@@ -0,0 +1,43 @@
+class Node:
+
+    def __init__(self, val):
+        self.val = val
+        self.left = self.right = None
+
+
+vals = list(map(int, open('input', 'r').read().split()))
+mod = len(vals)
+ans, zero, nums = 0, Node, [Node(vals[i] * 811589153) for i in range(mod)]
+for i in range(mod):
+    nums[i].left = nums[(i - 1) % mod]
+    nums[i].right = nums[(i + 1) % mod]
+mod -= 1
+for j in range(10):
+    for node in nums:
+        if node.val == 0:
+            zero = node
+            continue
+        if node.val % mod == 0:
+            continue
+        node.left.right = node.right
+        node.right.left = node.left
+        temp = node
+        if node.val > 0:
+            for i in range(node.val % mod):
+                temp = temp.right
+            node.right = temp.right
+            node.left = temp
+            temp.right.left = node
+            temp.right = node
+        else:
+            for i in range(-node.val % mod):
+                temp = temp.left
+            node.left = temp.left
+            node.right = temp
+            temp.left.right = node
+            temp.left = node
+for i in range(3):
+    for j in range(1000):
+        zero = zero.right
+    ans += zero.val
+print(ans)
