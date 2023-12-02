0bf9dcf686476c437a1929c720d645596994265e
TruVortex <75190116+TruVortex@users.noreply.github.com>
2023-12-02 08:35:26 -0500

    Update Star-1.py

--- 2023/Day 1/Star-1.py
+++ 2023/Day 1/Star-1.py
@@ -2,6 +2,6 @@ import re
 
 summate = 0
 for line in open('input', 'r'):
-    line = re.sub('\D', '', line)
+    line = re.sub(r'\D', '', line)
     summate += int(line[0]) * 10 + int(line[-1])
 print(summate)
