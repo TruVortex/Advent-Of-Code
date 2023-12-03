bda2c8d59f200fee75dfc49fd0a17952042613da
TruVortex <75190116+TruVortex@users.noreply.github.com>
2023-12-03 00:22:47 -0500

    Create Star-1.py

---
+++ 2023/Day 3/Star-1.py
@@ -0,0 +1,17 @@
+schematic = ['.' + line + '.' for line in open('input', 'r').read().splitlines()]
+schematic = ['.' * len(schematic[0])] + schematic + ['.' * len(schematic[0])]
+summate = 0
+for y in range(1, len(schematic) - 1):
+    cur = 0
+    part = False
+    for x in range(1, len(schematic[0])):
+        if schematic[y][x].isdigit():
+            cur *= 10
+            cur += int(schematic[y][x])
+        else:
+            if part:
+                summate += cur
+            cur = 0
+            part = False
+        part |= (cur > 0) and ((not schematic[y][x - 1].isdigit() and schematic[y][x - 1] != '.') or (not schematic[y][x + 1].isdigit() and schematic[y][x + 1] != '.') or (not schematic[y - 1][x].isdigit() and schematic[y - 1][x] != '.') or (not schematic[y + 1][x].isdigit() and schematic[y + 1][x] != '.') or (not schematic[y - 1][x - 1].isdigit() and schematic[y - 1][x - 1] != '.') or (not schematic[y + 1][x - 1].isdigit() and schematic[y + 1][x - 1] != '.') or (not schematic[y - 1][x + 1].isdigit() and schematic[y - 1][x + 1] != '.') or (not schematic[y + 1][x + 1].isdigit() and schematic[y + 1][x + 1] != '.'))
+print(summate)
