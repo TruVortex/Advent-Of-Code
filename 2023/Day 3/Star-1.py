b2e800ee32a23d4327b7fe3da17c12998e342b2a
TruVortex <75190116+TruVortex@users.noreply.github.com>
2023-12-03 00:23:04 -0500

    Update Star-1.py

--- 2023/Day 3/Star-1.py
+++ 2023/Day 3/Star-1.py
@@ -13,5 +13,5 @@ for y in range(1, len(schematic) - 1):
                 summate += cur
             cur = 0
             part = False
-        part |= (cur > 0) and ((not schematic[y][x - 1].isdigit() and schematic[y][x - 1] != '.') or (not schematic[y][x + 1].isdigit() and schematic[y][x + 1] != '.') or (not schematic[y - 1][x].isdigit() and schematic[y - 1][x] != '.') or (not schematic[y + 1][x].isdigit() and schematic[y + 1][x] != '.') or (not schematic[y - 1][x - 1].isdigit() and schematic[y - 1][x - 1] != '.') or (not schematic[y + 1][x - 1].isdigit() and schematic[y + 1][x - 1] != '.') or (not schematic[y - 1][x + 1].isdigit() and schematic[y - 1][x + 1] != '.') or (not schematic[y + 1][x + 1].isdigit() and schematic[y + 1][x + 1] != '.'))
+        part |= cur and ((not schematic[y][x - 1].isdigit() and schematic[y][x - 1] != '.') or (not schematic[y][x + 1].isdigit() and schematic[y][x + 1] != '.') or (not schematic[y - 1][x].isdigit() and schematic[y - 1][x] != '.') or (not schematic[y + 1][x].isdigit() and schematic[y + 1][x] != '.') or (not schematic[y - 1][x - 1].isdigit() and schematic[y - 1][x - 1] != '.') or (not schematic[y + 1][x - 1].isdigit() and schematic[y + 1][x - 1] != '.') or (not schematic[y - 1][x + 1].isdigit() and schematic[y - 1][x + 1] != '.') or (not schematic[y + 1][x + 1].isdigit() and schematic[y + 1][x + 1] != '.'))
 print(summate)
