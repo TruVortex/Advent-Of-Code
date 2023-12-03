089f86ba3c12942aab21f4c2593268f7908dd666
TruVortex <75190116+TruVortex@users.noreply.github.com>
2023-12-03 12:58:10 -0500

    Create Star-1-Revised.py

---
+++ 2023/Day 3/Star-1-Revised.py
@@ -0,0 +1,29 @@
+schematic = ['.' + line + '.' for line in open('input', 'r').read().splitlines()]
+schematic = ['.' * len(schematic[0])] + schematic + ['.' * len(schematic[0])]
+
+
+def get_num(yy, xx):
+    global schematic
+    if schematic[yy][xx] == '.':
+        return 0, -1, -1
+    while schematic[yy][xx].isdigit():
+        xx -= 1
+    xx += 1
+    start = xx
+    num = 0
+    while schematic[yy][xx].isdigit():
+        num *= 10
+        num += int(schematic[yy][xx])
+        xx += 1
+    return num, yy, start
+
+
+distinct = set()
+for y in range(1, len(schematic) - 1):
+    for x in range(1, len(schematic[0]) - 1):
+        if not schematic[y][x].isdigit() and schematic[y][x] != '.':
+            distinct.update([get_num(y, x - 1), get_num(y, x + 1), get_num(y - 1, x), get_num(y + 1, x), get_num(y - 1, x - 1), get_num(y + 1, x - 1), get_num(y - 1, x + 1), get_num(y + 1, x + 1)])
+summate = 0
+for part in distinct:
+    summate += part[0]
+print(summate)
