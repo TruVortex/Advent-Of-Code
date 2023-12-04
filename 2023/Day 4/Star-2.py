1f03927a95e084e0049ca4a0016223538c7dd91f
TruVortex <75190116+TruVortex@users.noreply.github.com>
2023-12-04 09:16:14 -0500

    Create Star-2.py

---
+++ 2023/Day 4/Star-2.py
@@ -0,0 +1,15 @@
+summate = 0
+cnt = [0] * 200
+for card, line in enumerate(open('input', 'r')):
+    numbers = line[line.find(':') + 1:].split(' | ')
+    winning, owned = list(map(int, numbers[0].split())), list(map(int, numbers[1].split()))
+    cnt[card] += 1
+    cur = 1
+    for num in winning:
+        if num in owned:
+            cur += 1
+    for i in range(1, cur):
+        if card + i < len(cnt):
+            cnt[card + i] += cnt[card]
+    summate += cnt[card]
+print(summate)
