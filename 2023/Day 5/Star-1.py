fd7b11c9d8ec6a591c3fb898c946cb2b654cc7cc
TruVortex <75190116+TruVortex@users.noreply.github.com>
2023-12-05 04:35:27 -0500

    Create Star-1.py

---
+++ 2023/Day 5/Star-1.py
@@ -0,0 +1,14 @@
+almanac = open('input', 'r').read().split('\n\n')
+seeds = list(map(int, almanac[0][6:].split()))
+for i in range(1, 8):
+    ranges = almanac[i].split('\n')
+    new = seeds.copy()
+    change = [False] * len(seeds)
+    for j in range(1, len(ranges)):
+        cur = list(map(int, ranges[j].split()))
+        for k in range(len(seeds)):
+            if not change[k] and cur[1] <= seeds[k] < cur[1] + cur[2]:
+                new[k] = seeds[k] + cur[0] - cur[1]
+                change[k] = True
+    seeds = new
+print(min(seeds))
