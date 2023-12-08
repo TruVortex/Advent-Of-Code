aef43128d887f6a5821d1769c075e0a25abf2f4c
TruVortex <75190116+TruVortex@users.noreply.github.com>
2023-12-07 19:09:41 -0500

    Update Star-1.py

--- 2021/Day 15/Star-1.py
+++ 2021/Day 15/Star-1.py
@@ -1,16 +1,16 @@
-from queue import Queue
+import heapq
 
 cavern = [[0] + list(map(int, line.strip())) + [0] for line in open('input', 'r')]
 cavern = [[0] * len(cavern[0])] + cavern + [[0] * len(cavern[0])]
 moves = ((0, -1), (0, 1), (-1, 0), (1, 0))
 vis = [[999999999] * len(cavern[0]) for _ in range(len(cavern))]
 vis[1][1] = 0
-q = Queue()
-q.put((1, 1, 0))
-while not q.empty():
-    cur = q.get()
+q = [(1, 1, 0)]
+while q:
+    cur = q[0]
+    heapq.heappop(q)
     for move in moves:
         if cavern[cur[0] + move[0]][cur[1] + move[1]] and cur[2] + cavern[cur[0] + move[0]][cur[1] + move[1]] < vis[cur[0] + move[0]][cur[1] + move[1]]:
             vis[cur[0] + move[0]][cur[1] + move[1]] = cur[2] + cavern[cur[0] + move[0]][cur[1] + move[1]]
-            q.put((cur[0] + move[0], cur[1] + move[1], cur[2] + cavern[cur[0] + move[0]][cur[1] + move[1]]))
+            heapq.heappush(q, (cur[0] + move[0], cur[1] + move[1], cur[2] + cavern[cur[0] + move[0]][cur[1] + move[1]]))
 print(vis[len(vis) - 2][len(vis[0]) - 2])
