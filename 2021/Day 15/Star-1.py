b167d1e4c0276dcdc70d3fba1ab94227841a2f20
TruVortex <75190116+TruVortex@users.noreply.github.com>
2023-12-07 19:19:31 -0500

    Update Star-1.py

--- 2021/Day 15/Star-1.py
+++ 2021/Day 15/Star-1.py
@@ -3,14 +3,17 @@ import heapq
 cavern = [[0] + list(map(int, line.strip())) + [0] for line in open('input', 'r')]
 cavern = [[0] * len(cavern[0])] + cavern + [[0] * len(cavern[0])]
 moves = ((0, -1), (0, 1), (-1, 0), (1, 0))
-vis = [[999999999] * len(cavern[0]) for _ in range(len(cavern))]
-vis[1][1] = 0
-q = [(1, 1, 0)]
+vis = [[0] * len(cavern[0]) for _ in range(len(cavern))]
+vis[1][1] = 1
+q = [(0, 1, 1)]
 while q:
     cur = q[0]
     heapq.heappop(q)
+    if cur[1] == len(vis) - 2 and cur[2] == len(vis[0]) - 2:
+        print(cur[0])
+        break
     for move in moves:
-        if cavern[cur[0] + move[0]][cur[1] + move[1]] and cur[2] + cavern[cur[0] + move[0]][cur[1] + move[1]] < vis[cur[0] + move[0]][cur[1] + move[1]]:
-            vis[cur[0] + move[0]][cur[1] + move[1]] = cur[2] + cavern[cur[0] + move[0]][cur[1] + move[1]]
-            heapq.heappush(q, (cur[0] + move[0], cur[1] + move[1], cur[2] + cavern[cur[0] + move[0]][cur[1] + move[1]]))
-print(vis[len(vis) - 2][len(vis[0]) - 2])
+        nx, ny = cur[1] + move[0], cur[2] + move[1]
+        if cavern[nx][ny] and not vis[nx][ny]:
+            vis[nx][ny] = 1
+            heapq.heappush(q, (cur[0] + cavern[nx][ny], nx, ny))
