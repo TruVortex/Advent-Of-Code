10aa9abc84a448a3276d7b263273888e48f6e4b6
TruVortex <75190116+TruVortex@users.noreply.github.com>
2023-12-08 20:24:57 -0500

    Update Star-1.py

--- 2015/Day 5/Star-1.py
+++ 2015/Day 5/Star-1.py
@@ -2,5 +2,5 @@ import re
 
 cnt = 0
 for string in open('input', 'r'):
-    cnt += len(''.join(re.findall(r'[aeiou]*', string))) >= 3 and len(re.findall(r'(.)\1', string)) > 0 and all(pair not in string for pair in ['ab', 'cd', 'pq', 'xy'])
+    cnt += len(re.split(r'[aeiou]', string)) > 3 and len(re.findall(r'(.)\1', string)) > 0 and all(pair not in string for pair in ['ab', 'cd', 'pq', 'xy'])
 print(cnt)
