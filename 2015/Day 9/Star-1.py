1d2edb53fc825829a4fe820f8f739102b2b799f0
TruVortex <75190116+TruVortex@users.noreply.github.com>
2023-12-09 13:10:02 -0500

    Create Star-1.py

---
+++ 2015/Day 9/Star-1.py
@@ -0,0 +1,12 @@
+import re
+from collections import defaultdict
+from itertools import permutations
+
+adj = defaultdict(dict)
+for line in open('input', 'r'):
+    u, v, dist = re.match(r'(\w+) to (\w+) = (\d+)', line).groups()
+    adj[u][v] = adj[v][u] = int(dist)
+ans = 999999999
+for permutation in list(permutations(adj)):
+    ans = min(ans, sum(adj[permutation[i]][permutation[i - 1]] for i in range(1, len(permutation))))
+print(ans)
