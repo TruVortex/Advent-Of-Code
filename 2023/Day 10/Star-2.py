bbe35fca9e4719a249602bd3f5a491ba6ff04aea
TruVortex <75190116+TruVortex@users.noreply.github.com>
2023-12-10 10:40:27 -0500

    Create Star-2.py

---
+++ 2023/Day 10/Star-2.py
@@ -0,0 +1,80 @@
+grid, start, cur, dirs = [], None, None, {'left': ['-', 'L', 'F'], 'right': ['-', 'J', '7'], 'down': ['|', 'L', 'J'], 'up': ['|', '7', 'F']}
+for i, line in enumerate(open('input', 'r')):
+    grid.append(['.'])
+    for j, char in enumerate(line.strip()):
+        if char == 'S':
+            start = [i + 1, j + 1]
+            cur = [i + 1, j + 1]
+        grid[-1].append(char)
+    grid[-1].append('.')
+grid = ['.' * len(grid[0])] + grid + ['.' * len(grid[0])]
+if grid[start[0]][start[1] - 1] in dirs['left'] and grid[start[0]][start[1] + 1] in dirs['right']:
+    grid[start[0]][start[1]] = '-'
+    cur[1] -= 1
+elif grid[start[0]][start[1]] in dirs['down'] and grid[start[0] - 1][start[1]] in dirs['up']:
+    grid[start[0] + 1][start[1]] = '|'
+    cur[0] += 1
+elif grid[start[0]][start[1] + 1] in dirs['right'] and grid[start[0] - 1][start[1]] in dirs['up']:
+    grid[start[0]][start[1]] = 'L'
+    cur[1] += 1
+elif grid[start[0]][start[1] - 1] in dirs['left'] and grid[start[0] - 1][start[1]] in dirs['up']:
+    grid[start[0]][start[1]] = 'J'
+    cur[1] -= 1
+elif grid[start[0]][start[1] - 1] in dirs['left'] and grid[start[0] + 1][start[1]] in dirs['down']:
+    grid[start[0]][start[1]] = '7'
+    cur[1] -= 1
+else:
+    grid[start[0]][start[1]] = 'F'
+    cur[1] += 1
+cnt, vis, loop = 1, [[False] * len(grid[0]) for _ in range(len(grid))], {tuple(cur)}
+vis[start[0]][start[1]] = vis[cur[0]][cur[1]] = True
+while cur != start:
+    if grid[cur[0]][cur[1]] == '-':
+        if vis[cur[0]][cur[1] - 1]:
+            cur[1] += 1
+        else:
+            cur[1] -= 1
+    elif grid[cur[0]][cur[1]] == '|':
+        if vis[cur[0] - 1][cur[1]]:
+            cur[0] += 1
+        else:
+            cur[0] -= 1
+    elif grid[cur[0]][cur[1]] == 'L':
+        if vis[cur[0]][cur[1] + 1]:
+            cur[0] -= 1
+        else:
+            cur[1] += 1
+    elif grid[cur[0]][cur[1]] == 'J':
+        if vis[cur[0]][cur[1] - 1]:
+            cur[0] -= 1
+        else:
+            cur[1] -= 1
+    elif grid[cur[0]][cur[1]] == '7':
+        if vis[cur[0]][cur[1] - 1]:
+            cur[0] += 1
+        else:
+            cur[1] -= 1
+    else:
+        if vis[cur[0]][cur[1] + 1]:
+            cur[0] += 1
+        else:
+            cur[1] += 1
+    cnt += 1
+    vis[cur[0]][cur[1]] = True
+    vis[start[0]][start[1]] = False
+    loop.add(tuple(cur))
+ans, grid = 0, [[grid[i][j] if (i, j) in loop else '.' for j in range(len(grid[0]))] for i in range(len(grid))]
+for i in range(len(grid)):
+    inside = up = False
+    for j, ch in enumerate(grid[i]):
+        if ch == '|':
+            inside = not inside
+        elif ch == 'L':
+            up = True
+        elif ch in 'J7':
+            if ch == ('7' if up else 'J'):
+                inside = not inside
+            up = False
+        elif ch == '.' and inside:
+            ans += 1
+print(ans)
