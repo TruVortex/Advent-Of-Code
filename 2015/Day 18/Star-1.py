ce3064dab57073dd27cebe3888d5dd4367183544
TruVortex <75190116+TruVortex@users.noreply.github.com>
2023-12-11 12:40:02 -0500

    Create Star-1.py

---
+++ 2015/Day 18/Star-1.py
@@ -0,0 +1,17 @@
+from copy import deepcopy
+
+grid = [list('.' + line.strip() + '.') for line in open('input', 'r')]
+grid, move = [['.'] * len(grid[0])] + grid + [['.'] * len(grid[0])], ((-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1))
+for _ in range(100):
+    new = deepcopy(grid)
+    for i in range(1, len(grid) - 1):
+        for j in range(1, len(grid[0]) - 1):
+            cnt = 0
+            for dy, dx in move:
+                cnt += grid[i + dy][j + dx] == '#'
+            if grid[i][j] == '#':
+                new[i][j] = '#' if 2 <= cnt <= 3 else '.'
+            else:
+                new[i][j] = '#' if cnt == 3 else '.'
+    grid = new
+print(sum(line.count('#') for line in grid))
