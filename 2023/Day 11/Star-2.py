d3b81aeb41d7e9cf9a7f8400ad3aada5672c8c9a
TruVortex <75190116+TruVortex@users.noreply.github.com>
2023-12-11 08:25:49 -0500

    Create Star-2.py

---
+++ 2023/Day 11/Star-2.py
@@ -0,0 +1,25 @@
+universe = [line.strip() for line in open('input', 'r')]
+rows, columns, galaxies = [], [], []
+for i, line in enumerate(universe):
+    if all(ch == '.' for ch in line):
+        rows.append(i)
+for i in range(len(universe[0])):
+    flag = True
+    for j in range(len(universe)):
+        flag &= universe[j][i] == '.'
+        if universe[j][i] == '#':
+            galaxies.append((j, i))
+    if flag:
+        columns.append(i)
+summate = 0
+for i in range(len(galaxies)):
+    for j in range(i + 1, len(galaxies)):
+        cnt = 0
+        for row in rows:
+            if min(galaxies[i][0], galaxies[j][0]) <= row <= max(galaxies[i][0], galaxies[j][0]):
+                cnt += 1
+        for column in columns:
+            if min(galaxies[i][1], galaxies[j][1]) <= column <= max(galaxies[i][1], galaxies[j][1]):
+                cnt += 1
+        summate += abs(galaxies[i][0] - galaxies[j][0]) + abs(galaxies[i][1] - galaxies[j][1]) + 999999 * cnt
+print(summate)
