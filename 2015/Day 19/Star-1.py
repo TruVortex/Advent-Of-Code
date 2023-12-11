bb5e2976522de9c738fa1daa070d8de300ca59a3
TruVortex <75190116+TruVortex@users.noreply.github.com>
2023-12-11 13:00:20 -0500

    Update Star-1.py

--- 2015/Day 19/Star-1.py
+++ 2015/Day 19/Star-1.py
@@ -3,6 +3,6 @@ distinct, medicine = set(), medicine.strip() + 'Q'
 for conversion in conversions.split('\n'):
     before, after = conversion.strip().split(' => ')
     for i in range(len(before), len(medicine)):
-        if medicine[i - len(before):i] == before:
+        if medicine[i - len(before):i] == before and medicine[i].isupper():
             distinct.add(medicine[:i - len(before)] + after + medicine[i:])
 print(len(distinct))
