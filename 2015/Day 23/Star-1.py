f532fd77de1950245f0661f3852f636fb458e915
TruVortex <75190116+TruVortex@users.noreply.github.com>
2023-12-12 14:56:26 -0500

    Create Star-1.py

---
+++ 2015/Day 23/Star-1.py
@@ -0,0 +1,17 @@
+cur, a, b, instructions = 0, 0, 0, [line.replace(',', '').split() for line in open('input', 'r')]
+while cur < len(instructions):
+    if instructions[cur][0][0] != 'j':
+        if instructions[cur][0] == 'hlf':
+            exec(f'{instructions[cur][1]} //= 2')
+        elif instructions[cur][0] == 'tpl':
+            exec(f'{instructions[cur][1]} *= 3')
+        else:
+            exec(f'{instructions[cur][1]} += 1')
+        cur += 1
+    elif instructions[cur][0] == 'jmp':
+        cur += int(instructions[cur][1])
+    elif instructions[cur][0] == 'jie':
+        exec(f'cur += max(1, (1 - {instructions[cur][1]} % 2) * {instructions[cur][2]})')
+    else:
+        exec(f'cur += max(1, ({instructions[cur][1]} == 1) * {instructions[cur][2]})')
+print(b)
