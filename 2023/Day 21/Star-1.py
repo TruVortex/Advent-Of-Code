dd1e7dc07ee474a6e2fd395751461192f584615e
TruVortex <75190116+TruVortex@users.noreply.github.com>
2023-12-21 15:58:27 -0500

    Update Star-1.py

--- 2023/Day 21/Star-1.py
+++ 2023/Day 21/Star-1.py
@@ -2,16 +2,16 @@ from collections import deque
 
 grid = ['#' + line.strip() + '#' for line in open('input', 'r')]
 grid = ['#' * len(grid[0])] + grid + ['#' * len(grid[0])]
-q = []
+q = None
 for y in range(len(grid)):
     for x in range(len(grid[0])):
         if grid[y][x] == 'S':
-            q.append((y, x))
+            q = [(y, x)]
 for _ in range(64):
     new_q = set()
     for cur in q:
         for dy, dx in ((0, -1), (0, 1), (-1, 0), (1, 0)):
             if grid[cur[0] + dy][cur[1] + dx] != '#':
                 new_q.add((cur[0] + dy, cur[1] + dx))
-    q = list(new_q)
+    q = new_q
 print(len(q))
