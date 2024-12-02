d0b1ef50521473c998ff9b6c758154285af61f3d
TruVortex <75190116+TruVortex@users.noreply.github.com>
2024-12-02 14:58:24 -0500

    Update Star-1.py

--- 2024/Day 2/Star-1.py
+++ 2024/Day 2/Star-1.py
@@ -1,9 +1,7 @@
-from math import copysign
-
 cnt = 0
 for line in open('input', 'r'):
     levels = list(map(int, line.split()))
-    positive = copysign(1, levels[1] - levels[0]) == 1
+    positive = levels[0] < levels[1]
     for i in range(1, len(levels)):
         if (positive and (levels[i] - levels[i - 1] < 1 or levels[i] - levels[i - 1] > 3)) or (not positive and (levels[i] - levels[i - 1] < -3 or levels[i] - levels[i - 1] > -1)):
             break
