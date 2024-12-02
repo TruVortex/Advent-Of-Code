84123f598cf0d3eedbe78c2035498d81e4e07b52
TruVortex <75190116+TruVortex@users.noreply.github.com>
2024-12-02 14:58:58 -0500

    Update Star-2.py

--- 2024/Day 2/Star-2.py
+++ 2024/Day 2/Star-2.py
@@ -5,7 +5,7 @@ for line in open('input', 'r'):
     levels = list(map(int, line.split()))
     for i in range(len(levels)):
         new_levels = levels[:i] + levels[i+1:]
-        positive = copysign(1, new_levels[1] - new_levels[0]) == 1
+        positive = new_levels[0] < new_levels[1]
         for j in range(1, len(new_levels)):
             if (positive and (new_levels[j] - new_levels[j - 1] < 1 or new_levels[j] - new_levels[j - 1] > 3)) or (not positive and (new_levels[j] - new_levels[j - 1] < -3 or new_levels[j] - new_levels[j - 1] > -1)):
                 break
